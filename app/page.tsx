51a0a92ce8a2a1a155811026941cc79bd8544d09
Sujal <sujaleither@gmail.com>
2026-08-03 19:43:26 +0530

    first change

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,89 @@
-import Image from "next/image";
+import { ToggleSection } from "@/components/ToggleSection";
+import { HeroSection } from "../components/sections/HeroSection";
+import { DraggableCardBody, DraggableCardContainer } from "@/components/DraggableCards";
+import LoveGallery from "@/components/LoveGallery";
+import StellarCardGallerySingle from "@/components/NewWorld";
+import Book from "@/components/Book";
+import { Section } from "lucide-react";
+
 
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+    <div className=" h-full w-full overflow-x-hidden overflow-y-scroll snap-y snap-mandatory bg-zinc-50 dark:bg-black select-none scroll-smooth">
+      {/* <section className="h-screen w-full snap-start snap-always shrink-0">
+        <HeroSection />
+      </section> */}
+    
+        {/* <ToggleSection /> */}
+        {/* <BirthdayMagic /> */}
+         
+           {/* <StellarCardGallerySingle/> */}
+           {/* <StellarCardGallerySingle/> */}
+             {/* <HeroSection /> */}
+             
+
+              <section  className="h-screen  w-full snap-start snap-always shrink-0 overflow-visible relative z-10" >
+                   <iframe
+      src="/birthday.html"
+      title="Birthday Reveal"
+      style={{
+        width: "100vw",
+        height: "100vh",
+        border: "none",
+        display: "block",
+      }}
+      allow="autoplay"
+    />  
+              </section>
+
+         
+
+         
+           
+        
+     
+ 
+    <section  className="h-screen  w-full snap-start snap-always shrink-0 overflow-visible relative z-10" >
+<HeroSection />
+    </section>
+
+
+      <section  className="h-screen  w-full snap-start snap-always shrink-0 overflow-visible relative z-10" >
+ <LoveGallery/>
+    </section>
+
+    <section  className="h-screen  w-full snap-start snap-always shrink-0 overflow-visible relative z-10" >
+ <div className="flex  h-screen overflow-hidden flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+                           <Book/>  
+              </div>
+    </section>
+
+
+
+    <section className="h-screen w-full snap-start overflow-y-scroll snap-always shrink-0 relative ">
+        <StellarCardGallerySingle />
+      </section>
+
+
+
+ 
+
+
+  
+
+
+    
+   
+
+
+    
+
+
+    
+
+     
+
+      
     </div>
   );
 }
